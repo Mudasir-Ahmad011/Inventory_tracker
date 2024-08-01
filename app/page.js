e40723a8edf7780e7b2eae016c0c968af4c0958f
Mudasir Ahmad <mudasirofficially@gmail.com>
2024-08-01 03:36:45 -0700

    UI changes

--- app/page.js
+++ app/page.js
@@ -62,7 +62,30 @@ const filteredInventory = inventory.filter(item =>
 );
 return (
     <>
-    <Box width="100vw" height="100vh" display="flex" flexDirection="column" justifyContent="center" alignItems="center" gap={2} bgcolor="lightgray">
+      <Box width="100vw" p={1} bgcolor="#2A4747" display="flex" flexDirection="row" justifyContent="space-between" alignItems="center"><Typography variant="h4" color="#61D095">Inventory Store</Typography>
+      <Stack width="400px" direction="row">
+       <Box width="100%" alignItems="center" display="flex" justifyContent="center" gap={1} color="#FFFFFF">
+          <TextField
+          variant="outlined"
+          placeholder="Search items"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          InputProps={{
+            style: { color: '#FFFFFF' },
+          }}
+          sx={{
+            '& .MuiOutlinedInput-root': {
+              '&:hover fieldset': {
+                borderColor: '#FFFFFF',
+              }
+            },
+          }}
+        />
+      <Button variant="contained" onClick={()=>{handleOpen()}}>Add New Item</Button>
+      </Box>
+      </Stack>
+      </Box>
+    <Box width="100vw" height="90vh"  display="flex" flexDirection="column" justifyContent="center" alignItems="center" gap={2} bgcolor="#61D095">
       <Modal open={open} onClose={handleClose}>
         <Box position="absolute" top="40%" left="40%" width={400} bgcolor="white" border="2px slid #000" p={4} boxShadow={24} display="flex" flexDirection="column" gap={3} sx={{tranform:"translate(-50%,-50%)"}}>
           <Typography variant="h6">Add Item</Typography>
@@ -72,30 +95,19 @@ return (
           </Stack>
         </Box>
       </Modal>
-      <Stack width="800px" height="100px">
-      <Box width="100%" minHeight="100px" display="flex" alignItems="center" justifyContent="center" gap={2}>
-      <TextField
-          variant="outlined"
-          placeholder="Search items"
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-          sx={{ marginBottom: 2 ,marginTop:2}}
-        />
-      <Button variant="contained" onClick={()=>{handleOpen()}}>Add New Item</Button>
-      </Box>
-      </Stack>
+      
       <Box border="1px solid #333">
         <Box
         width="800px"
         height="100px"
-        bgcolor="#ADD8E6"
+        bgcolor="#E0BAD7"
         display="flex"
         alignItems="center"
         justifyContent="center"
         >
           <Typography variant="h2" color="#333">Inventory Items</Typography>
         </Box>
-     <Stack width="800px" height="300px" spacing={1} overflow="auto">
+     <Stack width="800px" height="460px" spacing={0} overflow="auto">
       {filteredInventory.map(({name,quantity})=>(
         <Box key={name} width="100%" minHeight="150px" display="flex" alignItems="center" justifyContent="space-between" bgcolor="#f0f0f0" p={5}>
          <Typography variant="h3" color="#333" textAlign="center">
